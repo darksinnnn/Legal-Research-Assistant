e32e6a65107e8959f7f63e5bd0e0b50f722d33b9
Ashish Singh <73785073+darksinnnn@users.noreply.github.com>
2026-03-08 12:54:50 +0530

    Update firebase.js

--- Frontend/src/firebase.js
+++ Frontend/src/firebase.js
@@ -3,13 +3,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBAdmKN5M94b02Jd6uABE9N1ES3PVkVzf8",
-  authDomain: "legal-research-assistant-abc56.firebaseapp.com",
-  projectId: "legal-research-assistant-abc56",
-  storageBucket: "legal-research-assistant-abc56.firebasestorage.app",
-  messagingSenderId: "118807555691",
-  appId: "1:118807555691:web:9c0bd0896d85164c23cb2e",
-  measurementId: "G-G7VQL8HQ54"
+
 };
 
 const app = initializeApp(firebaseConfig);
